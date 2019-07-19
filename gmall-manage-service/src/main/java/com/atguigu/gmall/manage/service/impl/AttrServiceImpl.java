fc41eb216eb6cf927926dd30e82ab42ab186dea7
18838988676@163.com <git18838988676>
2019-07-19 21:06:53 +0800

    第三天结束； 总结如下： 搭建后台商品属性管理功能的web层和service层； 简单实现了商品分类属性的新增功能 gmall-manage-service、gmall-manage-web、gmall-api http://localhost:8081/index#

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/AttrServiceImpl.java
@@ -0,0 +1,36 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.BaseAttrInfo;
+import com.atguigu.gmall.bean.BaseAttrValue;
+import com.atguigu.gmall.manage.mapper.BaseAttrInfoMapper;
+import com.atguigu.gmall.manage.mapper.BaseAttrValueMapper;
+import com.atguigu.gmall.service.AttrService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class AttrServiceImpl implements AttrService {
+    @Autowired
+    BaseAttrInfoMapper baseAttrInfoMapper;
+
+    @Autowired
+    BaseAttrValueMapper baseAttrValueMapper;
+    @Override
+    public List<BaseAttrInfo> getAttrList(String catalog3Id) {
+        BaseAttrInfo baseAttrInfo = new BaseAttrInfo();
+        baseAttrInfo.setCatalog3Id(catalog3Id);
+        return baseAttrInfoMapper.select(baseAttrInfo);
+    }
+
+    @Override
+    public void saveAttr(BaseAttrInfo baseAttrInfo) {
+        baseAttrInfoMapper.insertSelective(baseAttrInfo);
+        List<BaseAttrValue> attrValueList = baseAttrInfo.getAttrValueList();
+        for (BaseAttrValue baseAttrValue:attrValueList){
+            baseAttrValue.setAttrId(baseAttrInfo.getId());
+            baseAttrValueMapper.insert(baseAttrValue);
+        }
+    }
+}
--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/IndexController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/IndexController.java
@@ -17,9 +17,6 @@ public class IndexController {
         return  "index";
     }
 
-    @RequestMapping("a")
-    public String a(){
-        return  "a";
-    }
+
 
 }
