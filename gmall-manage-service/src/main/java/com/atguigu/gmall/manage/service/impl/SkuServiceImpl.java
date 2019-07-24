67f2dd570d0c57739005aed9ead86c927b84fe2f
18838988676@163.com <git18838988676>
2019-07-24 12:06:29 +0800

    第8天中 redis缓存正常 并且成功使用redis缓存锁。下一步：ES

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
@@ -14,7 +14,7 @@ import com.atguigu.gmall.service.SkuService;
 import com.atguigu.gmall.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
-
+import org.apache.commons.lang3.StringUtils;
 import java.util.List;
 
 @Service
@@ -73,21 +73,74 @@ public class SkuServiceImpl implements SkuService {
 
     @Override
     public SkuInfo getSkuById(String skuId) {
-        Jedis jedis = redisUtil.getJedis();
+        Jedis jedis = null;
+        try {
+            jedis = redisUtil.getJedis();
+        }catch (Exception e){
+            return null;
+        }
         SkuInfo skuInfo = null;
 
         // 查询redis缓存
         String key = "sku:" + skuId + ":info";
         String val = jedis.get(key);
-        skuInfo = JSON.parseObject(val, SkuInfo.class);
 
-        if (skuInfo == null) {
-            // 查询db
-            skuInfo = getSkuByIdFormDb(skuId);
-
-            // 同步缓存
-            jedis.set(key, JSON.toJSONString(skuInfo));
+        if("empty".equals(val)){
+            System.out.println(Thread.currentThread().getName()+"发现数据库中暂时没有改商品，直接返回空对象");
+            return skuInfo;
         }
+
+
+        if (StringUtils.isBlank(val)) {
+            System.out.println(Thread.currentThread().getName()+"发现缓存中没有数据，申请分布式锁");
+            // 申请缓存锁
+            String OK = jedis.set("sku:" + skuId + ":lock", "1", "nx", "px", 5000);
+
+            if("OK".equals(OK)){// 拿到缓存锁
+
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(Thread.currentThread().getName()+"获得分布式锁，开始访问数据");
+                // 查询db
+                skuInfo = getSkuByIdFormDb(skuId);
+
+                if(skuInfo!=null){
+                    System.out.println(Thread.currentThread().getName()+"通过分布式锁，查询到数据，同步缓存");
+                    // 同步缓存
+                    jedis.set(key, JSON.toJSONString(skuInfo));
+
+                }else{
+                    // 通知同伴
+                    System.out.println(Thread.currentThread().getName()+"通过分布式锁，没有查询到数据，通知同伴在10秒之内不要访问该sku");
+                    jedis.setex("sku:" + skuId + ":info", 10,"empty");
+                }
+
+                // 归还缓存锁
+                System.out.println(Thread.currentThread().getName()+"归还分布式锁");
+                jedis.del("sku:" + skuId + ":lock");
+
+            }else{// 没有拿到缓存锁
+                // 自旋
+                System.out.println(Thread.currentThread().getName()+"发现分布式锁被占用，开始自旋");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                getSkuById(skuId);
+            }
+
+        }else{
+            // 正常转换缓存数据
+            System.out.println(Thread.currentThread().getName()+"正常从缓存中取得数据，返回结果");
+            skuInfo = JSON.parseObject(val, SkuInfo.class);
+        }
+
+
         return skuInfo;
     }
     private SkuInfo getSkuByIdFormDb(String skuId) {
